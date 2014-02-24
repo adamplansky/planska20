1e01df0226c4baa59a8be4580825712893f33267
plansada <plansada@fit.cvut.cz>
2014-02-24 16:17:31 +0100

    Structure added

---
+++ Planska20.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planska20.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Planska20.Web/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planska20.Web
+{
+    public class Class1
+    {
+    }
+}
